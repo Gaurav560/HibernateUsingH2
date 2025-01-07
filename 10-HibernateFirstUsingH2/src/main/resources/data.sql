0cbce59cbc93697fa9918f6adde4902885ccc4d5
gaurav <gsharma1154@gmail.com>
2025-01-07 15:47:21 +0530

    added data.sql file

---
+++ 10-HibernateFirstUsingH2/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- Insert some data into the StudentTable
+INSERT INTO StudentTable (SID, SNAME, SCITY) VALUES (101, 'Gaurav', 'Gaya');
+INSERT INTO StudentTable (SID, SNAME, SCITY) VALUES (102, 'Sushil', 'Sirohi');
+INSERT INTO StudentTable (SID, SNAME, SCITY) VALUES (108, 'Raj', 'Pune');
+INSERT INTO StudentTable (SID, SNAME, SCITY) VALUES (104, 'Priya', 'Bengaluru');
+INSERT INTO StudentTable (SID, SNAME, SCITY) VALUES (105, 'Vijay', 'Chennai');
